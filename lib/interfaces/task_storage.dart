eef682740ea5b3b5250bc664b1dd3f6aaa802094
Andassa <andassaramananandro@gmail.com>
2026-07-17 00:48:52 +0300

    feat: add abstractions, storage interface and UrgentTask

--- lib/interfaces/task_storage.dart
+++ lib/interfaces/task_storage.dart
@@ -1,18 +1,10 @@
 import '../models/task.dart';
 
-/// Contrat de stockage des taches.
-///
-/// Dart ne possede pas de mot-cle "interface". On utilise donc une classe
-/// abstraite dont les methodes n'ont pas de corps. Toute classe qui veut
-/// jouer le role de stockage devra implementer ce contrat et fournir une
-/// vraie logique pour chaque methode.
 abstract class TaskStorage {
-  /// Sauvegarde la liste complete des taches.
+
   Future<void> save(List<Task> tasks);
 
-  /// Charge et retourne toutes les taches disponibles.
   Future<List<Task>> load();
 
-  /// Supprime la tache qui correspond a l'identifiant donne.
   Future<void> delete(String id);
 }
